bdd64423ed441d4022d51f530f7069f38f5c2993
jd615645 <jd615645@gmail.com>
2017-08-01 22:30:38 +0800

    :cat2: rm comment

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
 const express = require('express')
 const router = express.Router()
-// const firebase = require('firebase')
-// const config = require('../config/config.json')
-
-// // init firebase
-// firebase.initializeApp(config)
-// const db = firebase.database()
-// const ref = db.ref('/')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
